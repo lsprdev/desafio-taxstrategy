2d3ed1da2bc89f9ea6027201df089a8827320b5e
lsprgabriel <ogabrielpereira@pm.me>
2024-02-24 17:09:05 -0300

    CHORE: Getting data from the add-order-form

--- src/app/components/modals/add-order-modal/add-order-modal.component.ts
+++ src/app/components/modals/add-order-modal/add-order-modal.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../buttons/button/button.component';
+import { Router } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-order-modal',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, FormsModule],
   templateUrl: './add-order-modal.component.html',
   styleUrl: './add-order-modal.component.css'
 })
 export class AddOrderModalComponent {
+  order = {
+    type: '',
+    description: '',
+    initialDate: '',
+    endDate: '',
+  };
+  constructor(private router: Router) { }
 
+  onSubmit(form: NgForm) {
+    console.log('Your form data : ', form.value);
+    this.router.navigateByUrl('dashboard');
+  }
 }
