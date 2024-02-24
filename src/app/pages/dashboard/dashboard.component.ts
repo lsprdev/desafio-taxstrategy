867ac0e59678d720c70d9871e78b680ecd4e4c04
lsprgabriel <ogabrielpereira@pm.me>
2024-02-24 11:44:10 -0300

    FEAT: Adding order table to the dashboard page.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { OrderTableComponent } from '../../components/order-table/order-table.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [OrderTableComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
