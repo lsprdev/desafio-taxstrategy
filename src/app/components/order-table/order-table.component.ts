d1b86144746721e10d7858e125bd55a8857d13d5
lsprgabriel <ogabrielpereira@pm.me>
2024-02-24 12:16:42 -0300

    CHORE: Adding @for to the table.

--- src/app/components/order-table/order-table.component.ts
+++ src/app/components/order-table/order-table.component.ts
@@ -8,5 +8,52 @@ import { Component } from '@angular/core';
   styleUrl: './order-table.component.css'
 })
 export class OrderTableComponent {
-
+  // Equipamento(TV, Celular, Desktop, Monitor, Notebook), Início, Previsão de  entrega, Problema apresentado, Funcionário responsável, Status.
+  orders = [
+    {
+      id: 1,
+      equipment: 'TV',
+      start: '2021-08-10',
+      delivery: '2021-08-15',
+      problem: 'Tela quebrada',
+      employee: 'João',
+      status: 'Em andamento'
+    },
+    {
+      id: 2,
+      equipment: 'Celular',
+      start: '2021-08-10',
+      delivery: '2021-08-15',
+      problem: 'Tela quebrada',
+      employee: 'João',
+      status: 'Em andamento'
+    },
+    {
+      id: 3,
+      equipment: 'Desktop',
+      start: '2021-08-10',
+      delivery: '2021-08-15',
+      problem: 'Tela quebrada',
+      employee: 'João',
+      status: 'Em andamento'
+    },
+    {
+      id: 4,
+      equipment: 'Monitor',
+      start: '2021-08-10',
+      delivery: '2021-08-15',
+      problem: 'Tela quebrada',
+      employee: 'João',
+      status: 'Em andamento'
+    },
+    {
+      id: 5,
+      equipment: 'Notebook',
+      start: '2021-08-10',
+      delivery: '2021-08-15',
+      problem: 'Tela quebrada',
+      employee: 'João',
+      status: 'Finalizado'
+    },
+  ];
 }
