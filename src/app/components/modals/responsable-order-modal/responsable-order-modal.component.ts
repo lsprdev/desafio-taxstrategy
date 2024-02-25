bf412d63f8c65347bb63c59cc8347d77549554e7
lsprgabriel <ogabrielpereira@pm.me>
2024-02-25 02:37:44 -0300

    FEAT: Adding Responsable Order Modal to the project

---
+++ src/app/components/modals/responsable-order-modal/responsable-order-modal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../buttons/button/button.component';
+
+@Component({
+  selector: 'app-responsable-order-modal',
+  standalone: true,
+  imports: [ButtonComponent],
+  templateUrl: './responsable-order-modal.component.html',
+  styleUrl: './responsable-order-modal.component.css'
+})
+export class ResponsableOrderModalComponent {
+
+}
